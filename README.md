3d1e9a16a4c5b281bf9ed513527a71f406145413
Emmanuel Dyan <emmanuel.dyan@inetprocess.com>
2017-08-10 16:41:50 +0200

    Beta4 for pypi (problem with desc)

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-# impulsare/config
-Version number: 1.0b1
+impulsare/config
+==================================
+Version number: 1.0b4
 
 Author: Emmanuel Dyan
 
 
-## Overview
+Overview
+==================================
 A config reader, that validates a YAML config file and add default values if required.
 
 Extra values won't be verified, that any component / library defines its own config parameters
@@ -13,14 +15,16 @@ in a single configuration file without blocking other to do the same.
 See `tests/static/` for examples.
 
 
-## Installation / Usage
+Installation / Usage
+==================================
 To install use pip:
 ```bash
 $ pip install --upgrade impulsare-config
 ```
 
 
-## Example
+Example
+==================================
 ```python
 from impulsare_config import Reader
 
@@ -35,7 +39,8 @@ config = Reader().parse(config_file, specs_file, default_file)
 ```
 
 
-## Development & Tests
+Development & Tests
+==================================
 ```bash
 $ pip install -r requirements.txt
 $ pip install -r requirements-dev.txt
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-__version__ = '1.0b1'
+__version__ = '1.0b4'
 
 here = path.abspath(path.dirname(__file__))
 
@@ -21,7 +21,10 @@ setup(
     name='impulsare-config',
     version=__version__,
     description='A config reader (yaml) that validates config files content.',
-    long_description=long_description,
+    long_description="""A config reader, that validates a YAML config file and add default values if required.
+
+Extra values won't be verified, that any component / library defines its own config parameters
+in a single configuration file without blocking other to do the same.""",
     url='https://github.com/impulsare/config',
     download_url='https://github.com/impulsare/config/tarball/' + __version__,
     license='AGPLv3',
@@ -33,7 +36,8 @@ setup(
     keywords='config,python,yaml',
     packages=find_packages(exclude=['docs', 'tests*']),
     include_package_data=True,
-    author='Emmanuel Dyan <emmanuel@impulsare.io>, Roel Bindels <roel@impulsare.io>',
+    author='Emmanuel Dyan',
+    author_email='emmanuel@impulsare.io',
     install_requires=install_requires,
     dependency_links=dependency_links
 )
